2de76e88cd7400aa69ea8c0c5e69c68daec753d7
evilblubb <brian.barnhart@in-focus.net>
2018-04-26 18:39:48 +0200

    added shape

--- opencvresize.py
+++ opencvresize.py
@@ -10,7 +10,16 @@ path ='/home/evilblubb/100EOS5D/'
 dic=[]
 def resizer(file):
     img = cv.imread(file)
-    res = cv.resize(img,None, fx =0.5, fy=0.5, interpolation=cv.INTER_CUBIC)
+    height, width = img.shape[:2]
+    dim = float(width)/float(height)
+    if dim > 1:
+        width=4000
+        height= 4000/dim
+        res = cv.resize(img, (int(width), int(height)), interpolation=cv.INTER_CUBIC)
+    else:
+        width=4000*dim
+        height= 4000
+        res = cv.resize(img, (int(width), int(height)), interpolation=cv.INTER_CUBIC)
     name=file.split('/')[-1]
     cv.imwrite('/home/evilblubb/100EOS5D/small/{}'.format(name), res)
 
