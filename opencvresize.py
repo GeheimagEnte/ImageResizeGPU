2837d369ccfe2dde5db05d2341c1343b977dfc62
evilblubb <brian.barnhart@in-focus.net>
2018-04-26 22:17:31 +0200

    reduced more code

--- opencvresize.py
+++ opencvresize.py
@@ -2,6 +2,7 @@
 import numpy as np
 import cv2 as cv
 import glob
+import threading
 from multiprocessing import Pool
 import time
 
@@ -10,20 +11,16 @@ dic=[]
 def resizer(file):
     img = cv.imread(file)
     height, width = img.shape[:2]
-
+    UMatimg=cv.UMat(img)
     dim = float(width)/float(height)
     if dim > 1:
         width=4000
         height= 4000/dim
-        start_time = time.time()
-        res = cv.resize(img, (int(width), int(height)), interpolation=cv.INTER_CUBIC)
-        print("--- %s seconds ---" % (time.time() - start_time))
+        res = cv.resize(UMatimg, (int(width), int(height)), interpolation=cv.INTER_CUBIC)
     else:
         width=4000*dim
         height= 4000
-        start_time = time.time()
-        res = cv.resize(img, (int(width), int(height)), interpolation=cv.INTER_CUBIC)
-        print("--- %s seconds ---" % (time.time() - start_time))
+        res = cv.resize(UMatimg, (int(width), int(height)), interpolation=cv.INTER_CUBIC)
     name=file.split('/')[-1]
     cv.imwrite('/home/evilblubb/100EOS5D/small/{}'.format(name), res)
 
@@ -31,6 +28,8 @@ for file in glob.glob(path+'*.JPG'):
     dic.append(file)
 
 if __name__ == "__main__":
-    pool=Pool(16)
+    start_time = time.time()
+    pool=Pool(96)
     pool.map(resizer, dic)
     pool.terminate()
+    print("--- %s seconds ---" % (time.time() - start_time))
