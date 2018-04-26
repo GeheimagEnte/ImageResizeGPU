0b66aacd9c001d99923917c45e4514cd62f11e6a
evilblubb <brian.barnhart@in-focus.net>
2018-04-26 17:45:22 +0200

    With Timemeasurment

--- opencvresize.py
+++ opencvresize.py
@@ -4,19 +4,22 @@ import cv2 as cv
 import glob
 import threading
 from multiprocessing import Pool
+import time
+
 path ='/home/evilblubb/100EOS5D/'
 dic=[]
 def resizer(file):
-    img = cv.UMat(cv.imread(file))
+    img = cv.imread(file)
     res = cv.resize(img,None, fx =0.5, fy=0.5, interpolation=cv.INTER_CUBIC)
     name=file.split('/')[-1]
-    print(name)
     cv.imwrite('/home/evilblubb/100EOS5D/small/{}'.format(name), res)
 
 for file in glob.glob(path+'*.JPG'):
     dic.append(file)
 
 if __name__ == "__main__":
-    pool=Pool(14)
+    start_time = time.time()
+    pool=Pool(16)
     pool.map(resizer, dic)
     pool.terminate()
+    print("--- %s seconds ---" % (time.time() - start_time))
