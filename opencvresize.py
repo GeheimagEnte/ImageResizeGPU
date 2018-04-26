ab6c7e1c3c1737e425b8642977732986f44470b7
Manuel Kosel <33829388+GeheimagEnte@users.noreply.github.com>
2018-04-26 09:42:29 +0200

    Initial commit
    A simple test for OpenCV resize

---
+++ opencvresize.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2 as cv
+import glob
+
+
+for file in glob.glob('/put/your/path/here/*.JPG'):
+    print(file)
+    img = cv.imread(file)
+    height, width = img.shape[:2]
+    res = cv.resize(img, ((int)(.5*width), (int)(.5*height)), interpolation=cv.INTER_CUBIC)
+    name=file.split('\\')[-1]
+    cv.imwrite('output/path/{}'.format(name), img)
+
