f909a4ffa2bb03849ec383c760bfa92e83c0aee6
evilblubb <brian.barnhart@in-focus.net>
2018-04-26 17:09:51 +0200

    Added Multiprocessing

--- opencvresize.py
+++ opencvresize.py
@@ -2,14 +2,22 @@
 import numpy as np
 import cv2 as cv
 import glob
-
-
-for file in glob.glob('/home/evilblubb/100EOS5D/*.JPG'):
+import threading
+from multiprocessing import Pool
+path ='/home/evilblubb/100EOS5D/'
+dic=[]
+def resizer(file):
     img = cv.UMat(cv.imread(file))
-    
-    imgUmat = cv.UMat(img)
-    res = cv.resize(imgUmat,None, fx =0.5, fy=0.5, interpolation=cv.INTER_CUBIC)
+    res = cv.resize(img,None, fx =0.5, fy=0.5, interpolation=cv.INTER_CUBIC)
     name=file.split('/')[-1]
     print(name)
     cv.imwrite('/home/evilblubb/100EOS5D/small/{}'.format(name), res)
 
+for file in glob.glob(path+'*.JPG'):
+    dic.append(file)
+
+
+if __name__ == "__main__":
+    pool=Pool(14)
+    pool.map(resizer, dic)
+    pool.terminate()
