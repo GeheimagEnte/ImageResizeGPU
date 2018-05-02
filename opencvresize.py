ffca0764a9f160f4e166ae964b6e60d4fb132d9c
evilblubb <brian.barnhart@in-focus.net>
2018-05-02 10:51:40 +0200

    small performance update

--- opencvresize.py
+++ opencvresize.py
@@ -6,29 +6,23 @@ from multiprocessing import Pool
 import time
 
 path ='/home/evilblubb/100EOS5D/'
-dic=[]
+dic = glob.glob(path+'*.JPG')
 def resizer(file):
     img = cv.imread(file)
     height, width = img.shape[:2]
-    UMatimg=cv.UMat(img)
     dim = float(width)/float(height)
     if dim > 1:
         width=4000
         height= 4000/dim
-        res = cv.resize(UMatimg, (int(width), int(height)), interpolation=cv.INTER_CUBIC)
+        res = cv.resize(img, (int(width), int(height)), interpolation=cv.INTER_CUBIC)
     else:
         width=4000*dim
         height= 4000
-        res = cv.resize(UMatimg, (int(width), int(height)), interpolation=cv.INTER_CUBIC)
+        res = cv.resize(img, (int(width), int(height)), interpolation=cv.INTER_CUBIC)
     name=file.split('/')[-1]
     cv.imwrite('/home/evilblubb/100EOS5D/small/{}'.format(name), res)
 
-for file in glob.glob(path+'*.JPG'):
-    dic.append(file)
-
 if __name__ == "__main__":
-    start_time = time.time()
     pool=Pool(16)
     pool.map(resizer, dic)
     pool.terminate()
-    print("--- %s seconds ---" % (time.time() - start_time))
