5626bbbe4ce2ae5a1be1a5936f0fb10bfee8e74e
evilblubb <brian.barnhart@in-focus.net>
2018-05-02 10:55:48 +0200

    GPU Version

--- opencvresize.py
+++ opencvresize.py
@@ -9,16 +9,17 @@ path ='/home/evilblubb/100EOS5D/'
 dic = glob.glob(path+'*.JPG')
 def resizer(file):
     img = cv.imread(file)
+    UMatimg = cv.UMat(img)
     height, width = img.shape[:2]
     dim = float(width)/float(height)
     if dim > 1:
         width=4000
         height= 4000/dim
-        res = cv.resize(img, (int(width), int(height)), interpolation=cv.INTER_CUBIC)
+        res = cv.resize(UMatimg, (int(width), int(height)), interpolation=cv.INTER_CUBIC)
     else:
         width=4000*dim
         height= 4000
-        res = cv.resize(img, (int(width), int(height)), interpolation=cv.INTER_CUBIC)
+        res = cv.resize(UMatimg, (int(width), int(height)), interpolation=cv.INTER_CUBIC)
     name=file.split('/')[-1]
     cv.imwrite('/home/evilblubb/100EOS5D/small/{}'.format(name), res)
 
