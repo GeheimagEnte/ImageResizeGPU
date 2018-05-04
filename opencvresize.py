d99dc8f35277cb54ef8192feac37f9eb3449fa35
Manuel Kosel <manuel@sportograf.com>
2018-05-04 12:11:06 +0200

    Added EXIF transfer to new file; Restored time measurement

--- opencvresize.py
+++ opencvresize.py
@@ -4,25 +4,29 @@ import cv2 as cv
 import glob
 from multiprocessing import Pool
 import time
+from piexif import transplant
 
-path ='/home/evilblubb/100EOS5D/'
+path ='D:\\4711_TEST GPU\\4711_originale\\HERO\\'
 dic = glob.glob(path+'*.JPG')
 def resizer(file):
-    img = cv.imread(file)
+    name = file.split('\\')[-1]
+    outFile = 'D:\\outputCVPy\\{}'.format(name)
+    img = cv.imread(file, cv.IMREAD_IGNORE_ORIENTATION | cv.IMREAD_ANYCOLOR)
     height, width = img.shape[:2]
     dim = float(width)/float(height)
     if dim > 1:
         width=4000
         height= 4000/dim
-        res = cv.resize(img, (int(width), int(height)), interpolation=cv.INTER_CUBIC)
     else:
         width=4000*dim
         height= 4000
-        res = cv.resize(img, (int(width), int(height)), interpolation=cv.INTER_CUBIC)
-    name=file.split('/')[-1]
-    cv.imwrite('/home/evilblubb/100EOS5D/small/{}'.format(name), res)
+    res = cv.resize(img, (int(width), int(height)), interpolation=cv.INTER_CUBIC)
+    cv.imwrite(outFile, res, [cv.IMWRITE_JPEG_QUALITY, 80])
+    transplant(file, outFile)
 
 if __name__ == "__main__":
     pool=Pool(16)
+    start_time = time.time()
     pool.map(resizer, dic)
     pool.terminate()
+    print("--- %s seconds ---" % (time.time() - start_time))
