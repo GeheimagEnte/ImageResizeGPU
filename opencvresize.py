5fcffd44184fb4cbcab395b2d579c8f41c3bbb0e
Manuel Kosel <manuel@sportograf.com>
2018-05-08 11:09:11 +0200

    Fixed orientation changed default quality to 90

--- opencvresize.py
+++ opencvresize.py
@@ -4,17 +4,14 @@ import cv2 as cv
 import glob
 from multiprocessing import Pool
 import time
-from piexif import transplant
 import argparse
-
-_path = 'D:\\4711_TEST GPU\\4711_originale\\HERO\\'
-
+import piexif
 
 def resizer(params):
     file = params['input']
     outFile = params['output']
     longside = params['longside']
-    img = cv.imread(file, cv.IMREAD_IGNORE_ORIENTATION | cv.IMREAD_ANYCOLOR)
+    img = cv.imread(file, cv.IMREAD_ANYCOLOR)
     height, width = img.shape[:2]
     dim = float(width) / float(height)
     if dim > 1:
@@ -25,7 +22,10 @@ def resizer(params):
         height = longside
     res = cv.resize(img, (int(width), int(height)), interpolation=cv.INTER_CUBIC)
     cv.imwrite(outFile, res, [cv.IMWRITE_JPEG_QUALITY, params['quality']])
-    transplant(file, outFile)
+    exif_dict = piexif.load(file)
+    if piexif.ImageIFD.Orientation in exif_dict["0th"]:
+        exif_dict["0th"][piexif.ImageIFD.Orientation] = 0
+        piexif.insert(piexif.dump(exif_dict), outFile)
 
 
 if __name__ == "__main__":
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     parser.add_argument("-t", "--threads", help="Number of concurrent resize threads. Default: 8", type=int, default=8)
     parser.add_argument("-l", "--longside", type=int, default=4000,
                         help="Number of pixels along the long side. The short side will be resized accordingly with the same factor. Default: 4000")
-    parser.add_argument("-q", "--quality", help="JPEG image quality. Default: 80", type=int, default=80)
+    parser.add_argument("-q", "--quality", help="JPEG image quality. Default: 80", type=int, default=90)
     args = parser.parse_args()
 
     if (not path.isdir(args.input)):
@@ -48,7 +48,6 @@ if __name__ == "__main__":
             outFile = path.join(args.output, path.relpath(file, start=args.input))
             if (not path.isdir(path.dirname(outFile))):
                 makedirs(path.dirname(outFile))
-            print(outFile)
             params.append(dict(input=file, output=outFile, longside=args.longside, quality=args.quality))
         start_time = time.time()
         pool.map(resizer, params)
